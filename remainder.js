a13fbe9556a404d442649c5e90bf88ea1f9039a2
Sheikh Riyadh <sheikhriyad350883@gmail.com>
2022-08-06 14:13:33 +0600

    Get remainder

---
+++ remainder.js
@@ -0,0 +1,9 @@
+/* Sarah’s mother is teaching her mathematics. She gave Sarah a number and asked her what the
+remainder would be if she divided the number by 5. Help Sarah write the program. */
+
+let sarahNumber = 119;
+let dividedNumber = 5;
+
+let resultOfRemainder = sarahNumber % dividedNumber;
+
+console.log("The remainder is:", resultOfRemainder);
